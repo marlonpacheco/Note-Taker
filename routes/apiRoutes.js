692b62e4a5964b4ba9632be4c5bfbfb2182879d7
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-09-20 15:05:25 -0500

    apiRoute.js was able to show html pages. work on saving notes next

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,21 @@
 const database = require("../db/db.json");
 const express = require("express");
+const path = require("path");
 const fs = require("fs");
 
-module.exports = function (app) {
-    // API GET Requests
 
-    app.get("/api/notes", function (req, res) {
-        res.json(database);
+module.exports = function(app){
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
-
-    app.post("/api/notes", function (req, res){
-
-    })
-    
+    app.get("/api/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
 }
+
+
+
+
+// app.post("/api/notes", function (req, res) {
+
+// })
