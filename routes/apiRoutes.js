eb34b606e60a4d745eeb426a93e0952f740beed3
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-09-20 15:22:07 -0500

    adding notes to db.json seems is now working. need to figure out why it is not displaying in html

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const database = require("../db/db.json");
 const express = require("express");
 const path = require("path");
+const uniqid = require('uniqid')
 const fs = require("fs");
 
 
@@ -11,6 +12,20 @@ module.exports = function(app){
     app.get("/api/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
+
+    app.post("/api/notes", function (req, res){
+        let title = req.body.title;
+        let text = req.body.text;
+        let newNote = {title,text, id:uniqid()};
+        let savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf-8"));
+        savedNotes.push(newNote)
+
+        fs.writeFileSync("db/db.json", JSON.stringify(savedNotes));
+
+        res.json(newNote)
+        console.log(newNote)
+    })
+
 }
 
 
